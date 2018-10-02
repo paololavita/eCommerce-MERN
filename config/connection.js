09b80c0860a4d474cfdb421545ffd49b60231e35
Jonas Grumby <plavita@hotmail.com>
2018-10-01 21:48:33 -0400

    Heroku Update

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
 module.exports = {
-    Connection: 'mongodb://localhost:27017/inventory'
+    //Connection: 'mongodb://localhost:27017/inventory'
+    Connection: MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/inventory"
 }
