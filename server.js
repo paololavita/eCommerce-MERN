f958709ee198ef7663b8ff839e1cee00c049ea58
lmarchet <leo.marchetti@hotmail.ca>
2018-10-10 15:17:34 -0400

    included checkout folder with STRIPE payment

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+const dotenv = require("dotenv").config();
+
+// ***** STRIPE PAYMENT ***** keyPublishable and keySecret identify developer account when you communicate with Stripe
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+const stripe = require("stripe")(keySecret);
+// ***** STRIPE PAYMENT ***** 
+
 
 // Require all models
-
 
 var db = require("./models");
 
@@ -25,7 +32,6 @@ var app = express();
 // app.use(express.static(path.join(__dirname+ 'public')));
 // app.use(express.static('public'));
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -36,48 +42,73 @@ const dbconnection = require('./config/Connection.js');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(dbconnection.Connection, {
- 
+
 }).then(() => {
-  console.log("Successfully connected to the database");    
+  console.log("Successfully connected to the database");
 }).catch(err => {
   console.log('Could not connect to the database. Exiting now...', err);
   process.exit();
 });
 
 
-app.get(`/api/look/prod/`,(req,res)=>{
-  
+app.get(`/api/look/prod/`, (req, res) => {
+
   db.Production.find({})
-    .then(result =>{res.json(result)})
-  
-  })
+    .then(result => { res.json(result) })
 
-app.post('/api/production', (req,res)=>{
+})
 
-    db.Production.create(req.body)
-        .then(function(dbproduction){
-          console.log("table created..");
-        })
-        .catch(function(err){
-          console.log(err.message)
-        });
+app.post('/api/production', (req, res) => {
+
+  db.Production.create(req.body)
+    .then(function (dbproduction) {
+      console.log("table created..");
+    })
+    .catch(function (err) {
+      console.log(err.message)
+    });
 
 });
 
-app.post('/api/user', (req,res)=>{
+app.post('/api/user', (req, res) => {
 
   db.Invuser.create(req.body)
-      .then(function(dbuser){
-        console.log("table created..");
-      })
-      .catch(function(err){
-        console.log(err.message)
-      });
+    .then(function (dbuser) {
+      console.log("table created..");
+    })
+    .catch(function (err) {
+      console.log(err.message)
+    });
 
 });
-// require('./routes/routes.js')(app);
+
+// //*** STRIPE PAYMENT CODE BEGIN **** Express server exposes a POST route that receives payment token an creates the charge
+
+// app.post("/charge", (req, res) => {
+//   let amount = 500;
+
+//   stripe.customers.create({
+//     email: req.body.email,
+//     card: req.body.id
+//   })
+//     .then(customer =>
+//       stripe.charges.create({
+//         amount,
+//         description: "Sample Charge",
+//         currency: "usd",
+//         customer: customer.id
+//       }))
+//     .then(charge => res.send(charge))
+//     .catch(err => {
+//       console.log("Error:", err);
+//       res.status(500).send({ error: "Purchase Failed" });
+//     });
+// });
+// // ***** STRIPE CODE END *****
+
+// // require('./routes/routes.js')(app);
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
